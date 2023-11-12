27dcb8641373cbb64e0b058371096cd484ddef86
kokocorona <68634519+kokocorona@users.noreply.github.com>
2023-11-12 21:22:38 +0200

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-"use client"
-
 import Link from "next/link"
 
 export default function Home() {
-  window.location.href = "/vod"
+
   return (
     <main className="container">
       <h1>Vod project</h1>
